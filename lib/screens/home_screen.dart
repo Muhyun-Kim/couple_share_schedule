b532d5c4c0fca4db83852fd278b08a019b31608e
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-03-14 20:04:18 +0900

    add auth page

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:couple_share_schedule/screens/auth_screen.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const AuthScreen();
+  }
+}
