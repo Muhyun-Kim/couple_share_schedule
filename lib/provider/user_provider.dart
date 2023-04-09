02789518733862ca7a7747f670b2e70e0d788760
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-04-09 20:53:48 +0900

    add user provider

--- lib/provider/user_provider.dart
+++ lib/provider/user_provider.dart
@@ -1,7 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final userProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
-final userIdProvider = Provider<String?>((ref) => ref.watch(userProvider).currentUser?.uid);
-final userNameProvider = Provider<String?>((ref) => ref.watch(userProvider).currentUser?.displayName);
-final userImgProvider = Provider<String?>((ref) => ref.watch(userProvider).currentUser?.photoURL);
+final userProvider = Provider<User>((ref) => FirebaseAuth.instance.currentUser!);
