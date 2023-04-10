551bfde21888a9a2de6a8db95a92c4004af4e994
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-04-11 07:32:47 +0900

    update schedule model

--- lib/models/schedule_list_model.dart
+++ lib/models/schedule_list_model.dart
@@ -1,42 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ScheduleListModel {
-  final String userId;
-  final String userName;
   final DateTime selectedDate;
-  final String startTime;
-  final String endTime;
-  final String scheduleTitle;
-  final DocumentReference reference;
+  final List scheduleInfo;
 
   ScheduleListModel({
-    required this.userId,
-    required this.userName,
     required this.selectedDate,
-    required this.startTime,
-    required this.endTime,
-    required this.scheduleTitle,
-    required this.reference,
+    required this.scheduleInfo,
   });
 
   factory ScheduleListModel.fromFireStore(
       DocumentSnapshot<Map<String, dynamic>> snapshot) {
     final data = snapshot.data()!;
+    final DateTime selectedDate = data['selectedDate'].toDate();
+    final DateTime selectedUtc =
+        DateTime.utc(selectedDate.year, selectedDate.month, selectedDate.day);
+    final scheduleInfo = data['scheduleInfo'];
     return ScheduleListModel(
-      userId: data['userId'] as String,
-      userName: data['userName'] as String,
-      selectedDate: data['selectedDate'] as DateTime,
-      startTime: data['startTime'] as String,
-      endTime: data['endTime'] as String,
-      scheduleTitle: data['scheduleTitle'] as String,
-      reference: snapshot.reference,
+      selectedDate: selectedUtc,
+      scheduleInfo: scheduleInfo,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       "selectedDate": selectedDate,
-      "scheduleInfo": [startTime, endTime, scheduleTitle],
+      "scheduleInfo": scheduleInfo,
     };
   }
 }
