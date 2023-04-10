b3c0d8246f7c6c24d0ef4c6ac1ae6a1f3abe4053
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-04-10 14:10:13 +0900

    get firestore test

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:couple_share_schedule/models/schedule_list_model.dart';
 import 'package:flutter/material.dart';
 
 class Test extends StatefulWidget {
-  const Test({super.key});
+  final CollectionReference<ScheduleListModel> schedulesReference;
+  const Test({Key? key, required this.schedulesReference}) : super(key: key);
 
   @override
   State<Test> createState() => _TestState();
@@ -10,9 +13,33 @@ class Test extends StatefulWidget {
 class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
+    CollectionReference schedule =
+        FirebaseFirestore.instance.collection('sZIdsUZ3roZqQ5MjEZqQSoHoVbG2');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Test"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot<ScheduleListModel>>(
+              stream: widget.schedulesReference.snapshots(),
+              builder: (context, snapshot) {
+                final docs = snapshot.data?.docs ?? [];
+                return ListView.builder(
+                  itemCount: docs.length,
+                  itemBuilder: (context, index) {
+                    final schedule = docs[index].data();
+                    return ListTile(
+                      title: Text(schedule.selectedDate.toString()),
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
