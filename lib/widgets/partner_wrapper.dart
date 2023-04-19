9dfdc539810ff602855571249abe1a0e5fa6576e
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-04-20 07:29:07 +0900

    modify partner_wrapper

--- lib/widgets/partner_wrapper.dart
+++ lib/widgets/partner_wrapper.dart
@@ -11,48 +11,22 @@ class PartnerWrapper extends StatefulWidget {
   State<PartnerWrapper> createState() => _PartnerWrapperState();
 }
 
+final userId = FirebaseAuth.instance.currentUser!.uid;
+final partnerStream =
+    FirebaseFirestore.instance.collection(userId).doc("partner").snapshots();
+
 class _PartnerWrapperState extends State<PartnerWrapper> {
-  Future getPartnerInfo() async {
-    final userId = FirebaseAuth.instance.currentUser!.uid;
-    final partnerSnapshot = await FirebaseFirestore.instance
-        .collection(userId)
-        .doc("partner")
-        .get();
-    final partnerInfo = partnerSnapshot.data();
-    if (partnerInfo == null) {
-      return null;
-    } else {
-      // print(partnerInfo["partnerUid"]);
-      return partnerInfo;
-    }
-  }
-
-  var partnerGetInfo;
-
-  @override
-  void initState() {
-    super.initState();
-    getPartnerInfo().then((value) {
-      partnerGetInfo = value;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    final userId = FirebaseAuth.instance.currentUser!.uid;
-    final partnerStream = FirebaseFirestore.instance
-        .collection(userId)
-        .doc("partner")
-        .snapshots();
-
-    return StreamBuilder<DocumentSnapshot>(
+    return StreamBuilder(
       stream: partnerStream,
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const CircularProgressIndicator();
         } else if (snapshot.hasData && snapshot.data!.exists) {
-          final partnerInfo = snapshot.data!.data();
           return const PartnerMainScreen();
+        } else if (snapshot.hasError) {
+          return Text("Error: ${snapshot.error}");
         } else {
           return const PartnerAddScreen();
         }
