140c93e94d2dbdb9954ce2282c27a70bec437a7f
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-05-13 14:06:30 +0900

    change lef menu ui

--- lib/widgets/left_menu.dart
+++ lib/widgets/left_menu.dart
@@ -1,7 +1,6 @@
 import 'package:couple_share_schedule/provider/user_provider.dart';
 import 'package:couple_share_schedule/screens/login_screen.dart';
 import 'package:couple_share_schedule/widgets/left_menu_widget/full_image_screen.dart';
-import 'package:couple_share_schedule/widgets/left_menu_widget/user_qrcode.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,7 +9,7 @@ import 'package:url_launcher/url_launcher.dart';
 class LeftMenu extends ConsumerStatefulWidget {
   LeftMenu({super.key, required this.currentUser});
 
-  User? currentUser;
+  final User? currentUser;
 
   @override
   ConsumerState<LeftMenu> createState() => _LeftMenuState();
@@ -50,115 +49,140 @@ class _LeftMenuState extends ConsumerState<LeftMenu> {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          DrawerHeader(
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 7, 202, 205),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(7),
+          SizedBox(
+            height: 270,
+            child: DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Color(0xFFC3E99D),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(7),
+                ),
               ),
-            ),
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const FullImageScreen(),
-                  ),
-                );
-              },
-              child: Column(
-                children: [
-                  SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                      image: NetworkImage(currentUserPhotoURL),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const FullImageScreen(),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          ListTile(
-            title: Text("ID: ${widget.currentUser!.uid}"),
-            onTap: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                context: context,
-                builder: (BuildContext buildContext) {
-                  return const UserQRCode();
-                },
-              );
-            },
-          ),
-          ListTile(
-            title: Text(currentUserName),
-            onTap: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return AlertDialog(
-                    title: const Text('ユーザー名を変更しますか？'),
-                    content: const Text(""),
-                    actions: <Widget>[
-                      TextField(
-                        controller: displayNameInput,
-                        decoration: InputDecoration(
-                          border: const OutlineInputBorder(),
-                          labelText: currentUserName,
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            child: const Text('変更'),
-                            onPressed: () async {
-                              Navigator.of(context).pop();
-                              await widget.currentUser!
-                                  .updateDisplayName(displayNameInput.text);
-                              final user = FirebaseAuth.instance.currentUser!;
-                              final userProvider =
-                                  ref.read(currentUserProvider.notifier);
-                              userProvider.setUser(user);
-                            },
-                          ),
-                          TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: Theme.of(context).textTheme.labelLarge,
-                            ),
-                            child: const Text('キャンセル'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      ),
-                    ],
                   );
                 },
-              );
-            },
+                child: Column(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: ClipOval(
+                        child: Image.network(
+                          currentUserPhotoURL,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          currentUserName,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text(
+                                    'ユーザー名を変更しますか？',
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  actions: <Widget>[
+                                    TextField(
+                                      controller: displayNameInput,
+                                      decoration: InputDecoration(
+                                        border: const OutlineInputBorder(),
+                                        labelText: currentUserName,
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        TextButton(
+                                          style: TextButton.styleFrom(
+                                            textStyle: Theme.of(context)
+                                                .textTheme
+                                                .labelLarge,
+                                          ),
+                                          child: const Text('変更'),
+                                          onPressed: () async {
+                                            Navigator.of(context).pop();
+                                            await widget.currentUser!
+                                                .updateDisplayName(
+                                                    displayNameInput.text);
+                                            final user = FirebaseAuth
+                                                .instance.currentUser!;
+                                            final userProvider = ref.read(
+                                                currentUserProvider.notifier);
+                                            userProvider.setUser(user);
+                                          },
+                                        ),
+                                        TextButton(
+                                          style: TextButton.styleFrom(
+                                            textStyle: Theme.of(context)
+                                                .textTheme
+                                                .labelLarge,
+                                          ),
+                                          child: const Text('キャンセル'),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          },
+                          icon: Icon(Icons.edit),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
           ListTile(
+            leading: Icon(
+              Icons.security_outlined,
+            ),
             title: Text("利用規約"),
             onTap: _opnePrivacyPolicy,
           ),
           ListTile(
+            leading: Icon(
+              Icons.logout_outlined,
+            ),
             title: const Text("ログアウト"),
             onTap: () => FirebaseAuth.instance.signOut(),
           ),
           ListTile(
+            iconColor: Colors.red,
+            textColor: Colors.red,
+            leading: Icon(
+              Icons.person_off_outlined,
+            ),
             title: const Text("アカウント削除"),
             onTap: () {
               showDialog(
                 context: context,
                 builder: (BuildContext context) {
                   return AlertDialog(
-                    title: Text("アカウントを削除確？"),
                     content: Text(
                         "アカウントを削除すると、すべてのデータが削除されます。削除するなら、「削除」と入力してください。"),
                     actions: <Widget>[
@@ -175,8 +199,24 @@ class _LeftMenuState extends ConsumerState<LeftMenu> {
                           TextButton(
                             onPressed: () async {
                               if (deleteAccountInput.text == "削除") {
-                                await widget.currentUser!.delete();
-                                widget.currentUser = null;
+                                await widget.currentUser!.delete().catchError(
+                                      (error) => showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return AlertDialog(
+                                            content: Text("削除するなら再ログインが必要です"),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: Text("OK"),
+                                              ),
+                                            ],
+                                          );
+                                        },
+                                      ),
+                                    );
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
@@ -187,7 +227,7 @@ class _LeftMenuState extends ConsumerState<LeftMenu> {
                                 Navigator.of(context).pop();
                               }
                             },
-                            child: Text("確認"),
+                            child: Text("削除"),
                           ),
                           TextButton(
                             onPressed: () {
