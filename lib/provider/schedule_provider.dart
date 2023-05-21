adc9378662005241d0ea19c699202883120dfff5
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-05-21 10:57:44 +0900

    schedule_provider

--- lib/provider/schedule_provider.dart
+++ lib/provider/schedule_provider.dart
@@ -2,13 +2,49 @@
 //Modified : 2023/05/19
 //Function : ログイン状態の時、最初表示される画面
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final currentUserUid = FirebaseAuth.instance.currentUser?.uid ?? "";
+
 final scheduleProvider =
-    StateNotifierProvider<ScheduleState, Map<DateTime, List<String>>?>(
+    StateNotifierProvider<ScheduleState, Map<DateTime, List<String>>>(
         (ref) => ScheduleState(ref));
 
-class ScheduleState extends StateNotifier<Map<DateTime, List<String>>?> {
-  ScheduleState(this.ref) : super(null);
+class ScheduleState extends StateNotifier<Map<DateTime, List<String>>> {
+  ScheduleState(this.ref) : super({});
   Ref ref;
+
+  void setSchedule(Map<DateTime, List<String>> schedule) {
+    state = schedule;
+    ref.invalidate(scheduleProvider);
+  }
+
+  var _scheduleMap = <DateTime, List<String>>{};
+  final currentUser = FirebaseAuth.instance.currentUser!;
+
+  Future<Map<DateTime, List<String>>> getSchedule() async {
+    final schduleListSnapshot = await FirebaseFirestore.instance
+        .collection(currentUser.uid)
+        .orderBy("selectedDate")
+        .get();
+    final schedule = schduleListSnapshot.docs.map((doc) {
+      return doc.data();
+    }).toList();
+    for (var i = 0; i < schedule.length; i++) {
+      final selectedDate = (schedule[i]['selectedDate'] as Timestamp).toDate();
+      final DateTime selectedUtc =
+          DateTime.utc(selectedDate.year, selectedDate.month, selectedDate.day);
+      final scheduleInfo =
+          schedule[i]['scheduleInfo'].cast<String>() as List<String>;
+      _scheduleMap[selectedUtc] = scheduleInfo;
+    }
+    return _scheduleMap;
+  }
+
+  void updateSchedule() async {
+    final value = await getSchedule();
+    setSchedule(value);
+  }
 }
