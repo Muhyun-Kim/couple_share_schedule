26817e3dd322a8576d7f39612d7581c14e95fc28
Muhyun-Kim <muhyun.kim.dev@gmail.com>
2023-06-16 20:09:31 +0900

    modify get my schedule but

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ import 'package:couple_share_schedule/widgets/home_widget/my_schedule_screen_bui
 import 'package:couple_share_schedule/widgets/left_menu.dart';
 import 'package:couple_share_schedule/widgets/left_menu_widget/full_image_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -79,7 +80,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       child: Text(
                         "my schedule",
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 16,
                           color: Color.fromARGB(255, 17, 20, 17),
                         ),
                       ),
@@ -88,7 +89,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       child: Text(
                         "partner schedule",
                         style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 16,
                           color: Color.fromARGB(255, 17, 20, 17),
                         ),
                       ),
